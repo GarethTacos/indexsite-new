9e263ec5f959c9935c1454cfc365a350ea555e30
holopocket <89393837+GarethTacos@users.noreply.github.com>
2025-09-18 22:14:15 +0800

    for ghpages2

--- build/_app/immutable/entry/start.CHAlUnah.js
+++
@@ -1,1 +0,0 @@
-import{l as o,a as r}from"../chunks/DBvs2YcA.js";export{o as load_css,r as start};
--- svelte.config.js
+++ svelte.config.js
@@ -1,23 +1,15 @@
 import adapter from '@sveltejs/adapter-static';
-import preprocess from 'svelte-preprocess';
 
-const config = {
-  // Use the 'preprocess' function you've imported
-  preprocess: preprocess(), 
-
+export default {
   kit: {
     adapter: adapter({
       pages: 'build',
       assets: 'build',
-      fallback: 'index.html',
-      precompress: false,
-      strict: true
+      fallback: 'index.html' // Optional: For SPA fallback
     }),
     paths: {
-      // You can set a base path if your site is hosted in a subdirectory
-      // base: '/my-static-site',
-    },
+      //base: '/<your-repo-name>' // e.g., '/indexsite-new'
+    }
   }
 };
 
-export default config;
