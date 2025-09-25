f81dd3710d2071cd1068eb93059f357df31a39c1
holopocket <89393837+GarethTacos@users.noreply.github.com>
2025-09-25 21:18:13 +0800

    build no work???

---
+++ build/_app/immutable/chunks/BqNgKnGH.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_fy66ic?.base??"/indexsite-new",e=globalThis.__sveltekit_fy66ic?.assets??s;export{e as a,s as b};
--- build/_app/immutable/chunks/DGTiAWrY.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_1cl8gc8?.base??"/indexsite-new",e=globalThis.__sveltekit_1cl8gc8?.assets??s;export{e as a,s as b};
