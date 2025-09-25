14f8fbe0512dd9b7c74877c2c1f8c89b06a61e45
holopocket <89393837+GarethTacos@users.noreply.github.com>
2025-09-25 19:42:55 +0800

    now mobile friendly!! (tweaked css)

--- src/lib/Hero.svelte
+++ src/lib/Hero.svelte
@@ -6,8 +6,8 @@ export let sub = "Main site of all the nonsensical stuffs I has.";
 <div id="header" style="text-align: center;"width="100%">
 
 	<br/>
-	<h1 style="background-color: white; font-size: 5em; opacity: .75;">{title}</h1>
-	<h2 style="background-color: white; font-size: 3em; opacity: .75;">{sub}</h2>
+	<h1 style="background-color: white; font-size: clamp(2rem, 6vw + 1rem, 5rem); opacity: .75;">{title}</h1>
+	<h2 style="background-color: white; font-size: clamp(1.5rem, 4vw + 1rem, 3rem); opacity: .75;">{sub}</h2>
 	<!--script src="/client-js/getver.js"></script-->
 
 </div>
